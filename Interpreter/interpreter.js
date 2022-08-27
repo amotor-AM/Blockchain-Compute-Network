43ab9b8c6afad96771b7ffc0ae4cdd4b9e14b407
amotor-AM <As64660@gmail.com>
2022-08-27 15:50:33 -0700

    finished the interpreter. Set up test suite to verify functionality

--- Interpreter/interpreter.js
+++ Interpreter/interpreter.js
@@ -11,7 +11,23 @@ const AND = "AND"
 const OR = "OR"
 const JUMP = "JUMP" // moves program counter to another location in the code array
 const JUMPI = "JUMPI" // jumps to destination IF a condition is met
-const EXECUTION_LIMIT = 100000
+const EXECUTION_LIMIT = 10000
+
+const opCodeMap = {
+    STOP : "STOP",
+    ADD: "ADD",
+    SUB: "SUB",
+    MUL: "MUL",
+    DIV: "DIV",
+    PUSH: "PUSH",
+    LT: "LT",
+    GT: "GT",
+    EQ: "EQ",
+    AND: "AND",
+    OR: "OR",
+    JUMP: "JUMP",
+    JUMPI: "JUMPI"
+}
 
 
 
@@ -39,6 +55,9 @@ class Interpreter {
                 throw new Error(`Execution Count Exceeded the Max Allowable Value of ${EXECUTION_LIMIT}.`) // prevents infinite loop crashes
             }
             const operationCode = this.state.code[this.state.programCounter]
+            if(!opCodeMap[operationCode]) {
+                throw new Error(`Operation code not supported: ${operationCode}`)
+            }
             try {
                 switch(operationCode) {
                     case STOP:
@@ -49,6 +68,7 @@ class Interpreter {
                             throw new Error("Invalid PUSH")
                         }
                         const value = this.state.code[this.state.programCounter]
+                        console.log(value)
                         this.state.stack.push(value)
                         break
                     case ADD:
@@ -62,15 +82,18 @@ class Interpreter {
                     case OR:
                         const firstValue = this.state.stack.pop()
                         const secondValue = this.state.stack.pop()
+                        if(typeof(firstValue) !== "number" || typeof(secondValue) !== "number") {
+                            throw new Error(`Type ${typeof(firstValue)} and ${typeof(secondValue)} are not comperable valid values`)
+                        }
                         if(operationCode === ADD) this.state.stack.push(firstValue + secondValue)
                         if(operationCode === SUB) this.state.stack.push(firstValue - secondValue)
                         if(operationCode === MUL) this.state.stack.push(firstValue * secondValue)
                         if(operationCode === DIV) this.state.stack.push(firstValue / secondValue)
-                        if(operationCode === LT) a < b ? this.state.stack.push(1) : this.state.stack.push(0) // binary yes / no 
-                        if(operationCode === GT) a > b ? this.state.stack.push(1) : this.state.stack.push(0)
-                        if(operationCode === EQ) a === b ? this.state.stack.push(1) : this.state.stack.push(0)
-                        if(operationCode === AND) this.state.stack.push(a && b)
-                        if(operationCode === OR) this.state.stack.push(a || b)
+                        if(operationCode === LT) firstValue < secondValue ? this.state.stack.push(1) : this.state.stack.push(0) // binary yes / no 
+                        if(operationCode === GT) firstValue > secondValue ? this.state.stack.push(1) : this.state.stack.push(0)
+                        if(operationCode === EQ) firstValue === secondValue ? this.state.stack.push(1) : this.state.stack.push(0)
+                        if(operationCode === AND) this.state.stack.push(firstValue && secondValue)
+                        if(operationCode === OR) this.state.stack.push(firstValue || secondValue)
                         break
                     case JUMPI:
                     case JUMP:
@@ -93,15 +116,5 @@ class Interpreter {
     }
 }
 
-// const codeArray = [PUSH, 2, PUSH, 3, SUB, STOP]
-
-// const interpreter = new Interpreter().runCode(codeArray)
-// console.log("result", interpreter)
-
-code = [PUSH, 0, JUMP, PUSH, 3, STOP]
-try {
-    new Interpreter().runCode(code)
-    console.log("result", result)
-} catch(err) {
-    console.log(err.message)
-}
+Interpreter.OPCODE_MAP = opCodeMap
+module.exports = Interpreter
---
+++ Interpreter/interpreter.test.js
@@ -0,0 +1,108 @@
+const Interpreter = require("./interpreter")
+const {STOP, ADD, SUB, MUL, DIV, PUSH, LT, GT, EQ, AND, OR, JUMP, JUMPI} = Interpreter.OPCODE_MAP
+
+
+describe("Interpreter",() => {
+    describe("runCode()", () => {
+        // computational
+        describe("and the code includes ADD", () => {
+            it("adds two values", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, ADD, STOP])
+                ).toEqual(5)
+            })
+        })
+        describe("and the code includes SUB", () => {
+            it("subtracts one value from another", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, SUB, STOP])
+                ).toEqual(1)
+            })
+        })
+        describe("and the code includes MUL", () => {
+            it("multiplies two values", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, MUL, STOP])
+                ).toEqual(6)
+            })
+        })
+        describe("and the code includes DIV", () => {
+            it("divides one value from another", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, DIV, STOP])
+                ).toEqual(1.5)
+            })
+        })
+        // comparison
+        describe("and the code includes GT", () => {
+            it("checks if one value is greater than another", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, GT, STOP])
+                ).toEqual(1)
+            })
+        })
+        describe("and the code includes LT", () => {
+            it("checks if one value is less than another", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, LT, STOP])
+                ).toEqual(0)
+            })
+        })
+        describe("and the code includes EQ", () => {
+            it("checks if one value is equal to another", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 2, PUSH, 3, EQ, STOP])
+                ).toEqual(0)
+            })
+        })
+        describe("and the code includes AND", () => {
+            it("ands two conditions", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 1, PUSH, 0, AND, STOP])
+                ).toEqual(0)
+            })
+        })
+        describe("and the code includes OR", () => {
+            it("ors two conditions", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 1, PUSH, 0, OR, STOP])
+                ).toEqual(1)
+            })
+        })
+        describe("and the code includes JUMP", () => {
+            it("jumps to a destination", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 6, JUMP, PUSH, 0, JUMP, PUSH, "jump successful", STOP])
+                ).toEqual("jump successful")
+            })
+        })
+        describe("and the code includes JUMPI", () => {
+            it("jumps to a destination", () => {
+                expect(
+                    new Interpreter().runCode([PUSH, 8, PUSH, 1, JUMPI, PUSH, 0, JUMP, PUSH, "jump successful", STOP])
+                ).toEqual("jump successful")
+            })
+        })
+        describe("and the code includes an invalid JUMP index", () => {
+            it("throws an error", () => {
+                expect(
+                    () => new Interpreter().runCode([PUSH, 99, JUMP, STOP]) //have to add callback function to catch errors that are thrown 
+                ).toThrow("Destination not valid: 99")
+            })
+        })
+        describe("and the code includes an invalid PUSH value", () => {
+            it("throws an error", () => {
+                expect(
+                    () => new Interpreter().runCode([PUSH, 0, PUSH])
+                ).toThrow("Invalid PUSH")
+            })
+        })
+        describe("and the code includes an infinite loop", () => {
+            it("throws an error", () => {
+                expect(
+                    () => new Interpreter().runCode([PUSH, 0, JUMP, STOP])
+                ).toThrow("Execution Count Exceeded the Max Allowable Value of 100000")
+            })
+        })
+    })
+})
